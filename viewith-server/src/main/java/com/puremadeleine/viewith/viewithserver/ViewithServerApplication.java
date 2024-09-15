ec1b64e0fcd3d85925d0dd61d38f61504258317f
zoyul <p.zoyul@gmail.com>
2024-09-15 16:14:59 +0900

    feat: add basic project

---
+++ viewith-server/src/main/java/com/puremadeleine/viewith/viewithserver/ViewithServerApplication.java
@@ -0,0 +1,13 @@
+package com.puremadeleine.viewith.viewithserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewithServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewithServerApplication.class, args);
+    }
+
+}
---
+++ viewith-server/src/test/java/com/puremadeleine/viewith/viewithserver/ViewithServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puremadeleine.viewith.viewithserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewithServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
