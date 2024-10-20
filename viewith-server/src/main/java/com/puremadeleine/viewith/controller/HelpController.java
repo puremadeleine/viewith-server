241a9ae8996140ba7c5280407c6e39511ac1c6ae
zoyul <p.zoyul@gmail.com>
2024-10-20 16:05:22 +0900

    feat: Add min, max value of page, size when getHelpList

--- viewith-server/src/main/java/com/puremadeleine/viewith/controller/HelpController.java
+++ viewith-server/src/main/java/com/puremadeleine/viewith/controller/HelpController.java
@@ -3,6 +3,8 @@ package com.puremadeleine.viewith.controller;
 import com.puremadeleine.viewith.dto.help.HelpInfoResDto;
 import com.puremadeleine.viewith.dto.help.HelpListResDto;
 import com.puremadeleine.viewith.service.HelpService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -22,8 +24,8 @@ public class HelpController {
     }
 
     @GetMapping("/list")
-    public HelpListResDto getReview(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                                    @RequestParam(value = "size", defaultValue = "10") Integer size,
+    public HelpListResDto getReview(@RequestParam(value = "page", defaultValue = "1") @Min(1) Integer page,
+                                    @RequestParam(value = "size", defaultValue = "10") @Min(1) @Max(20) Integer size,
                                     @RequestParam(value = "is_summary", required = false, defaultValue = "false") Boolean isSummary) {
         return helpService.getHelpList(page, size, isSummary);
     }
