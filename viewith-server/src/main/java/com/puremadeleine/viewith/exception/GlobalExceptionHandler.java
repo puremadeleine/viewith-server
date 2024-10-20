378ba959882c2df70c4eec21264667c403962f71
Hailie <de.hailie.v@gmail.com>
2024-10-20 12:31:39 +0900

    refactor: delete unused dto

--- viewith-server/src/main/java/com/puremadeleine/viewith/exception/GlobalExceptionHandler.java
+++ viewith-server/src/main/java/com/puremadeleine/viewith/exception/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.puremadeleine.viewith.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.http.HttpStatus;
 
 @Slf4j
 @RestControllerAdvice
@@ -18,7 +18,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("Exception", e);
+        log.error("[Exception] ", e);
         ErrorResponse errorResponse = ErrorResponse.of(ViewithErrorCode.UNKNOWN_EXCEPTION);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
