edeb1662e850b1a43bd944593137044b6da26dd8
zoyul <p.zoyul@gmail.com>
2024-10-31 03:04:04 +0900

    feat: Add health check controller

--- viewith-server/src/main/java/com/puremadeleine/viewith/controller/HealthController.java
+++ viewith-server/src/main/java/com/puremadeleine/viewith/controller/HealthController.java
@@ -1,2 +1,15 @@
-package com.puremadeleine.viewith.controller;public class HealthController {
+package com.puremadeleine.viewith.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping(value = {"health", "health-check", "healthcheck"})
+    public Map<String, String> healthCheck() {
+        return Map.of("health-check", "ok");
+    }
 }
