78f14df4933bce0289972f5b7dac212136ed0eb8
HailieDev <de.hailie.v@gmail.com>
2024-12-29 02:47:28 +0900

    feat: Add VenueSeatResDto

---
+++ src/main/java/com/puremadeleine/viewith/dto/venue/VenueSeatResDto.java
@@ -0,0 +1,21 @@
+package com.puremadeleine.viewith.dto.venue;
+
+import jakarta.annotation.Nullable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class VenueSeatResDto {
+    @Nullable
+    List<String> floors;
+    @Nullable
+    List<Integer> rows;
+    @Nullable
+    List<Integer> columns;
+}
