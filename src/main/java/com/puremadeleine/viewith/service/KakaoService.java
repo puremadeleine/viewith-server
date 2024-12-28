b99138a7b9cf103bc328ad9b50d528c9df0e20b3
HailieDev <de.hailie.v@gmail.com>
2024-12-29 00:53:02 +0900

    refactor: remove unused files and variables

--- src/main/java/com/puremadeleine/viewith/service/KakaoService.java
+++ src/main/java/com/puremadeleine/viewith/service/KakaoService.java
@@ -1,9 +1,7 @@
 package com.puremadeleine.viewith.service;
 
-import com.puremadeleine.viewith.config.client.KakaoOAuthProperties;
 import com.puremadeleine.viewith.dto.client.UserInfoResDto;
 import com.puremadeleine.viewith.repository.client.KakaoApiRepository;
-import com.puremadeleine.viewith.repository.client.KakaoOAuthRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -16,8 +14,6 @@ public class KakaoService {
     static String GRANT_TYPE = "authorization_code";
     static String TOKEN_PREFIX = "Bearer ";
 
-    KakaoOAuthProperties kakaoOAuthProperties;
-    KakaoOAuthRepository kakaoOAuthRepository;
     KakaoApiRepository kakaoApiRepository;
 
     public UserInfoResDto getAccessTokenInfo(String accessToken) {
