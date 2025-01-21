240c3db55362c51fbd84d630543737814e64f84c
HailieDev <de.hailie.v@gmail.com>
2025-01-21 23:58:34 +0900

    fix: VenueEntity svgUrl 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- tb_venue
-INSERT INTO tb_venue (venue_id, image_url, location, name)
-VALUES (1, 'http://example.com/image1.jpg', '서울시 구로구', '고척 스카이돔'),
-       (2, 'http://example.com/image2.jpg', '서울시 송파구', '잠실실내체육관'),
-       (3, 'http://example.com/image3.jpg', '서울시 중구', '장충체육관');
+INSERT INTO tb_venue (venue_id, image_url, location, name, svg_url)
+VALUES (1, 'http://example.com/image1.jpg', '서울시 구로구', '고척 스카이돔', 'http://example.com/svg.jpg'),
+       (2, 'http://example.com/image2.jpg', '서울시 송파구', '잠실실내체육관', 'http://example.com/svg.jpg'),
+       (3, 'http://example.com/image3.jpg', '서울시 중구', '장충체육관', 'http://example.com/svg.jpg');
 
 -- tb_seat
 INSERT INTO tb_seat (seat_id, seat_column, seat_row, venue_id, section, floor)
