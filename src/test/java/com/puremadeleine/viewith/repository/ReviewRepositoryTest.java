f49f70027c21a64106222d3d491c14b3b01bfe07
Hailie <de.hailie.v@gmail.com>
2025-02-14 00:37:07 +0900

    chore: test import문 추가

--- src/test/java/com/puremadeleine/viewith/repository/ReviewRepositoryTest.java
+++ src/test/java/com/puremadeleine/viewith/repository/ReviewRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Random;
