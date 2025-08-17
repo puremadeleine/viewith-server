224910418b49e7652e08c40fec2056955eda242a
zoyul <p.zoyul@gmail.com>
2025-08-17 17:29:36 +0900

    fix: Fetch Performance only when performance_id is provided during review creation

--- src/main/java/com/puremadeleine/viewith/service/ReviewService.java
+++ src/main/java/com/puremadeleine/viewith/service/ReviewService.java
@@ -26,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static com.puremadeleine.viewith.converter.review.ReviewServiceConverter.toReviewInfoResDto;
 import static com.puremadeleine.viewith.converter.review.ReviewServiceConverter.toReviewListResDto;
@@ -49,7 +50,11 @@ public class ReviewService {
         MemberEntity activeMember = memberProvider.getActiveMember(memberId);
         VenueEntity venue = venueProvider.getVenue(reqDto.venueId());
         SeatEntity seat = seatProvider.getSeat(venue.getId(), reqDto.section(), reqDto.seatRow(), reqDto.seatColumn());
-        PerformanceEntity performance = performanceProvider.getPerformance(reqDto.performanceId());
+
+        PerformanceEntity performance = null;
+        if (Objects.nonNull(reqDto.performanceId())) {
+            performanceProvider.getPerformance(reqDto.performanceId());
+        }
 
         ReviewEntity review = ReviewEntity.createReview(reqDto, venue, seat, performance, activeMember);
         ReviewEntity savedReview = reviewProvider.saveReview(review);
